437ecf00c7a4123199e95dd7477bbcb560b45af3
lmc2179 <louiscialdella@gmail.com>
2016-01-28 00:13:52 -0500

    I think it works!? Numerical stability issues

--- von_mises_fisher/von_mises_fisher.py
+++ von_mises_fisher/von_mises_fisher.py
@@ -5,6 +5,6 @@ from scipy.special import iv
 def vmf_pdf(x, mu, kappa):
     p = len(x) + 1
     likelihood = exp(kappa * np.dot(mu, x))
-    normalization_numerator = kappa ** (p/2 - 1)
-    normalization_denominator = 2 * pi ** (p/2) * iv(p/2-1, kappa)
+    normalization_numerator = kappa ** (0.5*p - 1)
+    normalization_denominator = 2 * pi ** (0.5*p) * iv(0.5*p-1, kappa)
     return likelihood * (normalization_numerator / normalization_denominator)
