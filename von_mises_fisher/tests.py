e1c84801ea74299bd1989c1d3abfe41ac61645a2
lmc2179 <louiscialdella@gmail.com>
2016-01-28 01:03:17 -0500

    what does it mean

--- von_mises_fisher/tests.py
+++ von_mises_fisher/tests.py
@@ -2,13 +2,17 @@ import unittest
 import numpy as np
 from von_mises_fisher.von_mises_fisher import vmf_pdf
 from math import cos, sin, acos, asin, sqrt, pi
+from matplotlib import pyplot as plt
 
 RIGHT_ANGLE = pi / 2
+
+def angle_to_vector(theta):
+    return np.array([cos(theta), sin(theta)])
 
 class PdfTest(unittest.TestCase):
     def test_2d_radial_symmetry(self):
         mu_angle = np.random.uniform(0, RIGHT_ANGLE)
-        mu = np.array([cos(mu_angle), sin(mu_angle)])
+        mu = angle_to_vector(mu_angle)
         kappa = 0.5
         max_angle = min(acos(mu[0]), RIGHT_ANGLE - acos(mu[0]))
         angle_perturb = np.random.uniform(0, max_angle)
@@ -17,8 +21,27 @@ class PdfTest(unittest.TestCase):
         x1 = np.array([cos(angle_1), sin(angle_1)])
         x2 = np.array([cos(angle_2), sin(angle_2)])
         print(mu_angle, max_angle, angle_perturb, angle_1, angle_2)
-        print(vmf_pdf(x1, mu, kappa))
-        print(vmf_pdf(x2, mu, kappa))
+        self.assertAlmostEqual(vmf_pdf(x1, mu, kappa), vmf_pdf(x2, mu, kappa), delta=0.01)
+
+    def test_monte_carlo_integration(self):
+        ITERATIONS = 10000
+        MU, KAPPA  = angle_to_vector(RIGHT_ANGLE / 2), 0.5
+        sum = 0
+        for i in range(ITERATIONS):
+            angle = np.random.uniform(0, 2*pi)
+            vector = angle_to_vector(angle)
+            sum += vmf_pdf(vector, MU, KAPPA)
+        print('Approximate integral: ', pi * (sum / ITERATIONS))
+
+    def test_plot_pdf(self):
+        mu_angle = np.random.uniform(0, RIGHT_ANGLE)
+        mu = angle_to_vector(mu_angle)
+        kappa = 100
+        theta = np.arange(mu_angle-pi, mu_angle+pi, 0.1)
+        V = [angle_to_vector(t) for t in theta]
+        y = [vmf_pdf(v, mu, kappa) for v in V]
+        plt.plot(theta, y)
+        plt.show()
 
 if __name__ == '__main__':
     unittest.main()
--- von_mises_fisher/von_mises_fisher.py
+++ von_mises_fisher/von_mises_fisher.py
@@ -3,7 +3,7 @@ from math import exp, pi
 from scipy.special import iv
 
 def vmf_pdf(x, mu, kappa):
-    p = len(x) + 1
+    p = len(x)
     likelihood = exp(kappa * np.dot(mu, x))
     normalization_numerator = kappa ** (0.5*p - 1)
     normalization_denominator = 2 * pi ** (0.5*p) * iv(0.5*p-1, kappa)
